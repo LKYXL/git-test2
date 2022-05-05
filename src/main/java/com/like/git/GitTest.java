ea9cb12869fd811883c04d6f48e9753f01dd722a
like <lkyxl99@gmail.com>
2022-05-05 23:13:08 +0800

    first commit

---
+++ src/main/java/com/like/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.like.git;
+
+/**
+ * @author like
+ * @create 2022-05-05-23:07
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git！");
+    }
+}
